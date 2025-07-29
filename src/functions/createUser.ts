1e8ad07ab447025c550562a3e083e7335a3f57d2
alexdiegoo <alex.diegosn10@gmail.com>
2025-07-29 20:48:27 -0300

    feat: refactor createUser function to use Zod for validation and improve response handling

--- src/functions/createUser.ts
+++ src/functions/createUser.ts
@@ -1,34 +1,43 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
 
 import { prisma } from "../database";
+import * as z from "zod";
 
-interface CreateUserRequest {
-    username: string;
-    email: string;
-}
+import {
+    createdResponse, 
+    badRequest, 
+    internalError 
+} from '../shared/http/responses';
+import { getErrorMessage } from '../shared/utils/getErrorMessage';
+
+const createUserSchema = z.object({
+    username: z.string().min(8).max(20),
+    email: z.email(),
+})
+
+type CreateUserRequest = z.infer<typeof createUserSchema>
 
 export async function handler(event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> {
     try {
         if (!event.body) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({ error: "User data is required" }),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            };
+            return badRequest('Request body is required');
         }
 
         const body = JSON.parse(event.body) as CreateUserRequest;
+        const parsedBody = createUserSchema.safeParse(body);
 
-        if (!body.username || !body.email) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({ error: "Username and email are required" }),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            };
+        if (!parsedBody.success) {
+            return badRequest(parsedBody.error.issues)
+        }
+
+        const userAlreadyExists = await prisma.user.findUnique({
+            where: {
+                email: body.email,
+            }
+        });
+
+        if (userAlreadyExists) {
+            return badRequest('User with this email already exists');
         }
 
         const user = await prisma.user.create({
@@ -38,24 +47,12 @@ export async function handler(event: APIGatewayProxyEventV2): Promise<APIGateway
             },
         });
 
-        return {
-            statusCode: 201,
-            body: JSON.stringify({
-                message: "User created successfully",
-                data: user
-            }),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        }
+        return createdResponse({
+            id: user.id,
+            username: user.name,
+            email: user.email,
+        });
     } catch(error) {
-        console.error("Error creating user:", error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: "Internal server error" }),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        };
+        return internalError(getErrorMessage(error));
     }
 }
